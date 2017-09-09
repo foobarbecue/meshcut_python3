679635f50371434d3b32eedeec5f2e4f580deb8b
Daniel Mansour <demansou@gmail.com>
2017-09-08 21:42:35 -0500

    updated xrange to range and print function to modern parenthesis-enclosed string

--- meshcut.py
+++ meshcut.py
@@ -37,7 +37,7 @@ class TriangleMesh(object):
         # Fill data structures
         for tid, f in enumerate(tris):
             tri_edges = []
-            for i in xrange(3):
+            for i in range(3):
                 v1 = f[i]
                 v2 = f[(i + 1) % 3]
                 e = make_edge(v1, v2)
@@ -225,7 +225,7 @@ def cross_section_mesh(mesh, plane, dist_tol=1e-8):
                                                        intersect, dist_tol)
 
                 if tid is None:
-                    print 'Degenerate case (probably non-closed mesh)'
+                    print('Degenerate case (probably non-closed mesh)')
                     break
 
             P.append(np.array(p))
